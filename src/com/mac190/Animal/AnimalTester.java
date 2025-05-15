b6ab9e6c29e236cb290aa193677a3f3493df1c38
Tao Su <96sutao@gmail.com>
2025-05-15 14:34:13 -0400

    class work

--- src/com/mac190/Animal/AnimalTester.java
+++ src/com/mac190/Animal/AnimalTester.java
@@ -1,4 +1,18 @@
 package com.mac190.Animal;
 
 public class AnimalTester {
+    public static void main(String[] args) {
+        // created an object Animal that is a Dog, age 3, color gray
+        Animal animal = new Animal("Canine", "Omnival", "Gray", 3);
+        System.out.println(animal);
+        Dog dog = new Dog("Gray", 3, "Chiwawa");
+        System.out.println(dog);
+        Cat cat = new Cat("Gray", 3, "Ciamoi");
+        System.out.println(cat);
+
+
+        Cat c1 = (Cat) animal;
+        System.out.println(c1);
+
+    }
 }
