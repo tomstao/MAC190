1ab7818739ac2b3e3826a7151fef736323cf9d02
Tao Su <96sutao@gmail.com>
2025-09-16 11:28:53 -0400

    stack

---
+++ src/com/MAC286/stacks/OurStack.java
@@ -0,0 +1,19 @@
+package com.MAC286.stacks;
+/*
+Stack is a data structure in which when you add something
+to store it get to the top of the stack and when you ermove the top comes out first
+
+LIFO: last in first out
+The standard interface for a stack is the following:
+- A default constructore
+- Size()
+ returns the number of the lements in the stack
+ - isEmpty() return trues if stack is empty, false if not
+ _ vod push(T e) adds e to the top of the stack
+ - T pop() removes the top of the stack in the stack
+ - T peel() returns the top the stack without removing it
+ - toString
+  */
+public class OurStack {
+
+}
