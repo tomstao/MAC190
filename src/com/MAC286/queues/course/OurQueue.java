e438c0e4d6e17021f102846efe8d8afc44729d95
Tao Su <96sutao@gmail.com>
2025-09-18 11:51:03 -0400

    queue

---
+++ src/com/MAC286/queues/course/OurQueue.java
@@ -0,0 +1,75 @@
+package com.MAC286.queues.course;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class OurQueue <T>{
+    //define a reference to an array of Ts
+    private T[] queue;
+    //size
+    private int size;
+
+    private T front;
+    private T rear;
+    //default constructor
+    public OurQueue(){
+        queue = (T[]) new Object[10];
+        size = 0;
+    }
+    //getter for size
+    public int size(){
+        return size;
+    }
+    //isEmpty
+    public boolean isEmpty(){
+        return (size == 0);
+    }
+    private void resize(){
+        queue = Arrays.copyOf(queue, queue.length*2);
+    }
+    //add(T e) add e to the back of the queue
+    public void add(T item){
+        if(size == queue.length){
+            resize();
+        }
+        //add item at the back
+        queue[size] = item;
+        size++;
+        this.rear = item;
+    }
+    //T remove() removes the first in the queue and returns it
+    public T remove(){
+        if(this.isEmpty()){
+            throw new NoSuchElementException();
+        }
+        //save the first
+        T save = queue[0];
+        for(int i = 0; i < size-1; i++){
+            queue[i] = queue[i+1];
+        }
+        size--;
+        return save;
+    }
+    //T peek() returns the first in the queue without removing it
+    public T peek(){
+        if(this.isEmpty()){
+            throw new NoSuchElementException();
+        }
+        return queue[0];
+    }
+    //String toString()
+    public String toString(){
+        if(this.isEmpty()){
+            return "[]";
+        }
+        String str = "[" + queue[0];
+        for(int i =1; i < size; i++){
+            str += ", "+ queue[i];
+        }
+        str += "]";
+
+        //One solution not advisable
+        //str = str.replace(", ]", "]");
+        return str;
+    }
+}
---
+++ src/com/MAC286/queues/course/QueueTester.java
@@ -0,0 +1,26 @@
+package com.MAC286.queues.course;
+
+//Change this tester to make it work with our queue
+public class QueueTester {
+    public static void main(String[] args) {
+//        OurQueue<Integer> Q = new OurQueue<>();
+//        System.out.println("size: " + Q.size() + " S: " + Q);
+//        Q.add(-1);
+//        Q.add(-3);
+//        System.out.println("size: " + Q.size() + " S: " + Q);
+//        Q.add(-9);
+//        System.out.println("size: " + Q.size() + " S: " + Q);
+//        System.out.println("The first in line is: " + Q.peek());
+//        System.out.println("size: " + Q.size() + " S: " + Q);
+//        System.out.println("rtemoving: " + Q.remove());
+//        System.out.println("size: " + Q.size() + " S: " + Q);
+//        Q.remove();
+//        Q.remove();
+//        System.out.println("size: " + Q.size() + " S: " + Q);
+//        Q.remove();
+
+        OurQueue<Integer> q = new OurQueue<>();
+
+
+    }
+}
